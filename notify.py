e482441849ed87711e9b50c1e8a4f8dd8df629ea
Shon Hay Paz <spaz@spaz.tlv.csb>
2020-03-21 21:33:42 +0200

    added http push endpoint support, fixed syntax and efficiency

--- notify.py
+++ notify.py
@@ -23,9 +23,10 @@ class Notifier:
         parser.add_argument('-a', '--access-key', help='access key for s3 object storage', required=True)
         parser.add_argument('-s', '--secret-key', help='secret key for s3 object storage', required=True)
         parser.add_argument('-b', '--bucket-name', help='s3 bucket name', required=True)
-        parser.add_argument('-k', '--kafka-endpoint', help='kafka endpoint in which rgw will send notifications to', required=False)
-        parser.add_argument('-r', '--rabbitmq-endpoint', help='rabbitmq topic in which rgw will send notifications to', required=False)
-        parser.add_argument('-t', '--topic', help='kafka topic in which rgw will send notifications to', required=True)
+        parser.add_argument('-ke', '--kafka-endpoint', help='kafka endpoint in which rgw will send notifications to', required=False)
+        parser.add_argument('-ae', '--amqp-endpoint', help='amqp endpoint in which rgw will send notifications to', required=False)
+        parser.add_argument('-he', '--http-endpoint', help='http endpoint in which rgw will send notifications to', required=False)
+        parser.add_argument('-t', '--topic', help='topic name in which rgw will send notifications to', required=True)
 
 
         # parsing all arguments
@@ -37,7 +38,8 @@ class Notifier:
         self.secret_key = args.secret_key
         self.bucket_name = args.bucket_name
         self.kafka_endpoint = args.kafka_endpoint
-        self.rabbitmq_endpoint = args.rabbitmq_endpoint
+        self.http_endpoint = args.http_endpoint
+        self.amqp_endpoint = args.amqp_endpoint
         self.topic = args.topic
         self.sns = boto3.client('sns', 
                                endpoint_url=self.endpoint_url, 
@@ -54,7 +56,7 @@ class Notifier:
                               config=botocore.client.Config(signature_version = 's3'))
 
 
-    ''' This function creates and sns-like topic with configured MQ endpoint'''
+    ''' This function creates and sns-like topic with configured push endpoint'''
     def create_sns_topic(self):
         
         # in case wanted MQ endpoint is kafka 
@@ -62,41 +64,39 @@ class Notifier:
             endpoint_args = 'push-endpoint=kafka://' + self.kafka_endpoint + '&kafka-ack-level=broker'
         
         # in case wanted MQ endpoint is rabbitmq
-        elif(self.rabbitmq_endpoint): 
-            endpoint_args = 'push-endpoint=amqp://' + rabbitmq_endpoint + '&amqp-exchange=' + exchange_name + '&amqp-ack-level=broker'
+        elif(self.amqp_endpoint): 
+            endpoint_args = 'push-endpoint=amqp://' + self.amqp_endpoint + '&amqp-exchange=' + self.exchange_name + '&amqp-ack-level=broker'
        
+        # in case wanted MQ endpoint is rabbitmq
+        elif(self.http_endpoint):
+            endpoint_args = 'push-endpoint=' + self.http_endpoint 
+
         # in case wanted MQ endpoint is not provided by the user 
         else:
-            raise Exception("please configure MQ endpoint!")
+            raise Exception("please configure a push endpoint!")
 
         # parsing given args to attributes 
         attributes = {nvp[0]: nvp[1] for nvp in urllib.parse.parse_qsl(endpoint_args, keep_blank_values=True)}
         
         # creates the wanted sns-like topic on RGW
-        self.sns.create_topic(Name=self.topic, Attributes=attributes)
+        self.topic_arn = self.sns.create_topic(Name=self.topic, Attributes=attributes)['TopicArn']
 
-    ''' This finction confiogures bucket notification for object creation and removal '''
+    ''' This function configures bucket notification for object creation and removal '''
     def configure_bucket_notification(self): 
         
         # creates a bucket if not exists
         self.s3.create_bucket(Bucket = self.bucket_name)
 
-        # generates the topic ARN based on the provided topic
-        topic_arn = "arn:aws:sns:default::" + self.topic 
-
         # initial dictionary 
         bucket_notifications_configuration = {
-            'TopicConfigurations': [
+            "TopicConfigurations": [
                 {
-                    'Id': 'test-configuration',
-                    'TopicArn': '',
-                    'Events': ['s3:ObjectCreated:*', 's3:ObjectRemoved:*']
+                    "Id": "configuration",
+                    "TopicArn": self.topic_arn,
+                    "Events": ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
                 }
             ]
         }
-
-        # updated the topic name provided as variable 
-        bucket_notifications_configuration['TopicConfigurations'][0]['TopicArn'] = topic_arn
 
         # pushed the notification configuration to the bucket 
         self.s3.put_bucket_notification_configuration(Bucket = self.bucket_name,
